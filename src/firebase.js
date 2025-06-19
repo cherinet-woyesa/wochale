524de90a011a89db4badfe4646cc2010a5f8cd0c
cherinet-woyesa <woyesabizunesh@gmail.com>
2025-06-19 14:53:00 +0300

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -5,11 +5,11 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "gfdhjfdgbd",
-  authDomain: "wochale-platform.firebaseapp.com",
-  projectId: "wochale-platform",
-  storageBucket: "wochale-platform.firebasestorage.app",
-  messagingSenderId: "632318340917",
-  appId: "1:632318340917:web:ed44b9ba5cd3048273e67c",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
